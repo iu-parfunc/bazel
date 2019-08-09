650d10c3fe4c526ef9653251ad1f9268bdf508b2
Ryan Scott <ryan.gl.scott@gmail.com>
2019-08-09 16:34:20 -0400

    Test wibbles

--- tests/test-determinism.sh
+++ tests/test-determinism.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set +x
 
-TARGET=$1
-BAZEL=bazel
+DETTRACE=$1
+BAZEL=$2
+TARGET=$3
 
 for suffix in {first,second}; do
-  ${BAZEL} clean && ${BAZEL} build ${TARGET} && hashdeep -lr bazel-genfiles/ > out.${suffix}
+  DETTRACE=${DETTRACE} ${BAZEL} clean && DETTRACE=${DETTRACE} ${BAZEL} build ${TARGET} && hashdeep -lr bazel-genfiles/ > out.${suffix}
 done
 
 hashdeep -ravvl -k out.first bazel-genfiles/
