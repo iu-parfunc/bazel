163dc8e646de8f54a64c3db6e47b141b2dfbd2ea
Ryan Scott <ryan.gl.scott@gmail.com>
2019-08-09 16:34:20 -0400

    Test wibbles

--- tests/nondet-genrule/main/spawn_evil.sh
+++ tests/nondet-genrule/main/spawn_evil.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EVIL=`head -c 10 /dev/urandom`
+EVIL=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo "#ifndef EVIL_H"
 echo "#define EVIL_H"
 echo "const char *evil = \"${EVIL}\";"
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set +x
 
+DETTRACE=$1
+BAZEL=$2
+
 TEST_DIR=`pwd`
 
 run_test() {
   DIR=$1
   TARGET=$2
-  ( cd ${DIR}; ${TEST_DIR}/test-determinism.sh ${TARGET} )
+  ( cd ${DIR}; ${TEST_DIR}/test-determinism.sh ${DETTRACE} ${BAZEL} ${TARGET} )
 }
 
 run_test "nondet-genrule" "//main:nondet-genrule"
