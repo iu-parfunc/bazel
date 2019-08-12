c344a15b188be8ca4165cb0b20b55eeba310bdee
Ryan Scott <ryan.gl.scott@gmail.com>
2019-08-12 13:59:48 -0400

    Run regression tests taken from outside sources
    This just builds abseil-cpp for now. More projects to come later.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -9,7 +9,36 @@ TEST_DIR=`pwd`
 run_test() {
   DIR=$1
   TARGET=$2
+
+  echo "Now running ${DIR} with \`bazel build ${TARGET}\`..."
   ( cd ${DIR}; ${TEST_DIR}/test-determinism.sh ${DETCMD} ${BAZEL} ${TARGET} )
 }
 
+# Try Bazel builds taken from the wild. We download them to avoid having to
+# check them in as submodules.
+run_external_test() {
+  GITHUB_USER=$1
+  GITHUB_REPO=$2
+  GITHUB_SHA_ABBREV=$3
+  TARGET=$4
+
+  GITHUB_TARBALL_URL="https://github.com/${GITHUB_USER}/${GITHUB_REPO}/tarball/${GITHUB_SHA_ABBREV}"
+  GITHUB_TARBALL_NAME="${GITHUB_USER}-${GITHUB_REPO}-${GITHUB_SHA_ABBREV}"
+  GITHUB_TARBALL_FILE="${GITHUB_TARBALL_NAME}.tar.gz"
+  DIR=${GITHUB_REPO}/${GITHUB_TARBALL_NAME}
+
+  if [ ! -f "${GITHUB_TARBALL_FILE}" ]; then
+    echo "Downloading ${GITHUB_TARBALL_FILE} from ${GITHUB_TARBALL_URL}..."
+    wget -O ${GITHUB_TARBALL_FILE} ${GITHUB_TARBALL_URL}
+  fi
+  if [ ! -d "${DIR}" ]; then
+    echo "Extracting ${GITHUB_TARBALL_FILE} to ${DIR}..."
+    mkdir -p ${GITHUB_REPO}
+    tar -xvf ${GITHUB_TARBALL_FILE} -C ${GITHUB_REPO}
+  fi
+
+  run_test "${DIR}" "${TARGET}"
+}
+
 run_test "nondet-genrule" "//main:nondet-genrule"
+run_external_test "abseil" "abseil-cpp" "52e88ee" "//absl/..."
