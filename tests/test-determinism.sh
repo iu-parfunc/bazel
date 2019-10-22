b300484399c9258be7dfc085b4388748da45eb37
Ryan Scott <ryan.gl.scott@gmail.com>
2019-10-22 12:53:59 -0400

    Use bazel-bin, not bazel-genfiles

--- tests/test-determinism.sh
+++ tests/test-determinism.sh
@@ -9,7 +9,7 @@ BAZEL_BUILD_ARGS=$4
 for suffix in {first,second}; do
   DETCMD=${DETCMD}   ${BAZEL} clean && \
     DETCMD=${DETCMD} ${BAZEL} build ${TARGET} ${BAZEL_BUILD_ARGS} && \
-    hashdeep -lr bazel-genfiles/ > out.${suffix}
+    hashdeep -lr bazel-bin/ > out.${suffix}
 done
 
-hashdeep -ravvl -k out.first bazel-genfiles/
+hashdeep -ravvl -k out.first bazel-bin/
