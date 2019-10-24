39b47ae47bce3fc0711d6423ace6f3cff811f6bb
Ryan Scott <ryan.gl.scott@gmail.com>
2019-10-24 14:05:54 -0400

    test-determinism.sh: Use bazel info bazel-bin

--- tests/test-determinism.sh
+++ tests/test-determinism.sh
@@ -7,10 +7,12 @@ BAZEL=$3
 TARGET=$4
 BAZEL_BUILD_ARGS=$5
 
+BAZEL_BIN_DIR=`DETCMD=${DETCMD} DETCMD_ARGS=${DETCMD_ARGS} ${BAZEL} info bazel-bin`
+
 for suffix in {first,second}; do
   DETCMD=${DETCMD}   DETCMD_ARGS=${DETCMD_ARGS} ${BAZEL} clean && \
     DETCMD=${DETCMD} DETCMD_ARGS=${DETCMD_ARGS} ${BAZEL} build ${TARGET} ${BAZEL_BUILD_ARGS} && \
-    hashdeep -lr bazel-bin/ > out.${suffix}
+    hashdeep -lr ${BAZEL_BIN_DIR} > out.${suffix}
 done
 
-hashdeep -ravvl -k out.first bazel-bin/
+hashdeep -ravvl -k out.first ${BAZEL_BIN_DIR}
